2f100d29ce4f77aa198ec4031a5e7afca37e4dac
impactlearner <impactlearner2020@gmail.com>
2020-08-04 18:01:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+const array = ["Ada", "Brendan", "Ali"];
+function writeCards(array) {
+    for (let i = 0; i < array.length; i++) {
+       console.log (`Happy birthday, ${array[i]}!`);
+        }
+        return array;
+}
+writeCards(array);
